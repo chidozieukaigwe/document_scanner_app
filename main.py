50548d5c045ef291b7965914a5aebfd4b95325e3
chido <chidodesigns@gmail.com>
2025-08-05 13:34:39 +0100

    Add scanner route and template page

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/scanner', methods=['GET'])
+def scanner():
+    return render_template('scanner.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

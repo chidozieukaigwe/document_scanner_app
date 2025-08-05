f992ab027c85b0f972e0d53cf16ea4d430285128
chido <chidodesigns@gmail.com>
2025-08-05 13:05:15 +0100

    add routes for index and about endpoints - add template pages for corresponding routes

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'document_scanner_app'
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
